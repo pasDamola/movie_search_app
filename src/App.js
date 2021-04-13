11435ecad2099c03cc10a83b40deb48bc9537153
Oyinda David <oyindadavid18@gmail.com>
2021-04-13 22:31:37 +0100

    style title added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Movie Search App</h1>
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
     </div>
   );
 }
