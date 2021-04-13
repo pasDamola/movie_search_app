e8a6258acf1ed1f8a05b1af6313d0feecca51c93
Oyinda David <oyindadavid18@gmail.com>
2021-04-14 00:03:58 +0100

    created MoviesDisplay component

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import MoviesDisplay from './MoviesDisplay'
 
 
 function SearchMovies () {
     const [query, setQuery] = useState('')
     const [movies, setMovies] = useState([])
-    
+
     const searchMovies = async (e) => {
         e.preventDefault();
         
@@ -30,23 +31,8 @@ function SearchMovies () {
                     />
                 <button className="button" type="submit">Search</button>
             </form>
-            <div className="card-list">
-                {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image"
-                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                            alt={movie.title + ' poster'}
-                            />
-                        <div className="card--content">
-                        <h3 className="card--title">{movie.title}</h3>
-                        <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                        <p><small>RATING: {movie.vote_average}</small></p>
-                        <p className="card--desc">{movie.overview}</p>
-                        </div>
-
-                    </div>
-                ))}
-            </div>    
+            <MoviesDisplay movies={movies}/>
+   
     </>
     )
 }
